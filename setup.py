c6acdc3f65c47368bf1217cd909ea7019b8c4204
kanalkun54 <kanalkun54@gmail.com>
2025-09-25 10:42:22 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
+import os
 
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("pip install requests")
+    os.system("pip install click")
+    os.system("pip install fake_headers")
+    os.system("pip install colorama")
+    os.system("pip install fade")
+elif c == "1":
+    os.system("pip3 install requests")
+    os.system("pip3 install click")
+    os.system("pip3 install fake_headers")
+    os.system("pip3 install colorama")
+    os.system("pip3 install fade")
+   
+print("Done.")
